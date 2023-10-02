1ce2edc4aae7beac1b82e71c18b6c31644936866
Meryem Kalkan <127223707+meryem-kalkan@users.noreply.github.com>
2023-10-02 23:38:53 +0400

    add navigation to searchbar

--- src/features/searchBar/searchBar.jsx
+++ src/features/searchBar/searchBar.jsx
@@ -3,9 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { setSearchValue } from './searchSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export const SearchBar = () => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
    const searchValue = useSelector(state => state.search.searchValue);
    const handleSearchValue = (e) => {
       dispatch(setSearchValue(e.target.value));
@@ -14,10 +16,16 @@ export const SearchBar = () => {
     dispatch(setSearchValue(''))
    }
 
+   const handleSubmit = (event) => {
+    event.preventDefault()
+    navigate(`/search?q=${searchValue}`)
+    dispatch(setSearchValue(''))
+   }
+
     return (
-        <div className='searchBar'>
+            <form onSubmit={handleSubmit} className='searchBar'>
             <input type='text' placeholder='search' value={searchValue} onChange={handleSearchValue}/>
             <FontAwesomeIcon icon={faCircleXmark } className='icons' onClick={clearSearchValue}/>
-        </div>
+            </form>
     )
 }
