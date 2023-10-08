7e97aeef1d7df05c2c7f8508c0376a418bb7e636
Meryem Kalkan <127223707+meryem-kalkan@users.noreply.github.com>
2023-10-08 11:58:52 +0400

    update comments.jsx

--- src/features/comments/comments.jsx
+++ src/features/comments/comments.jsx
@@ -4,31 +4,37 @@ import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchComments } from "./commentsSlice";
 import { Comment } from "../comment/comment";
+import { getFormattedNumber } from "../../utils/getFormattedData";
 
 export const Comments = () => {
     const dispatch = useDispatch();
     const { pathname } = useLocation();
     const comment = useSelector(state => state.comment);
     const post = useSelector(state => state.comment.post);
+    const comment_num = getFormattedNumber(post.comment);
 
      useEffect(() => {
       dispatch(fetchComments(pathname));
-     }, [])
+     }, []);
 
-    return (
+       if (comment.loading) return (
+       <div>loading ...</div>
+       );
+
+       if (!comment.loading && comment.error) return (
+        <div>error: {comment.error}</div>
+       )
+            
+       if (!comment.loading && comment.comments.length > 0) return (
         <section>
-           <Post post={post} key={post.id}/>
-           <section>
-            {comment.loading && <div>loading ...</div>}
-            {!comment.loading && comment.error ? <div>error: {comment.error}</div> : null}
-            {!comment.loading && comment.comments.length > 0 ? (
-                comment.comments.map(comment => {
-                    return (
-                        <Comment comment={comment} key={comment.id}/>
-                    )
-                })
-            ) : null} 
-           </section>
+         <Post post={post}/>   
+         <section>
+         <h2>Comments ({comment_num})</h2>
+         {comment.comments.map(comment => {
+                    return  <Comment comment={comment} key={comment.id}/>
+                })}
+         </section>
         </section>
-    )
+       )
+       
 }
